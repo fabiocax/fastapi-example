c4210929213b35f3babe841bd756fc37b2e71ca4
Fabio Alberti <fabiocax@gmail.com>
2021-08-13 15:21:06 -0300

    teste

--- example/main.py
+++ example/main.py
@@ -5,6 +5,7 @@ from models import Base , engine, SessionLocal, Users
 from sqlalchemy.orm import sessionmaker
 from os import environ
 from dependencies import *
+import requests
 
 example = APIRouter(
     prefix="/example/v1",
@@ -18,3 +19,9 @@ example = APIRouter(
 async def compare_texts(text1: str = Form(...),text2: str = Form(...), current_user: User = Depends(get_current_active_user)):
     ret=text1+" "+text2+" On user:"+str(current_user)
     return ret
+
+
+@example.post("/address/{cep}")
+async def search_address_by_cep(cep: str):
+    response = requests.get(f"http://viacep.com.br/ws/{cep}/json/")
+    return response.json()
